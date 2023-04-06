7301fa4fc1fd7f2d793f39a6d7de3cfe96bc27db
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-04-06 09:37:55 -0500

    base: project setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<div className="bg-red-400 items-center">
+			<h1 className="text-center text-3xl font-bold underline">Hello World!</h1>
+		</div>
+	);
+}
